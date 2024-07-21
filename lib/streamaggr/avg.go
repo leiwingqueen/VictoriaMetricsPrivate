7441dd8f1a41f2c74f72beb0786dd2b1b297e718
leiwingqueen <leiwingqueen@gmail.com>
2024-07-21 10:46:56 +0800

    implement stream aggregation

--- lib/streamaggr/avg.go
+++ lib/streamaggr/avg.go
@@ -2,8 +2,6 @@ package streamaggr
 
 import (
 	"sync"
-
-	"github.com/VictoriaMetrics/VictoriaMetrics/lib/fasttime"
 )
 
 // avgAggrState calculates output=avg, e.g. the average value over input samples.
@@ -23,62 +21,20 @@ func newAvgAggrState() *avgAggrState {
 }
 
 func (as *avgAggrState) pushSamples(samples []pushSample) {
-	for i := range samples {
-		s := &samples[i]
-		outputKey := getOutputKey(s.key)
-
-	again:
-		v, ok := as.m.Load(outputKey)
-		if !ok {
-			// The entry is missing in the map. Try creating it.
-			v = &avgStateValue{
-				sum:   s.value,
-				count: 1,
-			}
-			vNew, loaded := as.m.LoadOrStore(outputKey, v)
-			if !loaded {
-				// The entry has been successfully stored
-				continue
-			}
-			// Update the entry created by a concurrent goroutine.
-			v = vNew
-		}
-		sv := v.(*avgStateValue)
-		sv.mu.Lock()
-		deleted := sv.deleted
-		if !deleted {
-			sv.sum += s.value
-			sv.count++
-		}
-		sv.mu.Unlock()
-		if deleted {
-			// The entry has been deleted by the concurrent call to flushState
-			// Try obtaining and updating the entry again.
-			goto again
-		}
-	}
+	// TODO: implement pushSamples
+	// hint:
+	// - get the output key from the sample key
+	// - get the value from the map
+	// - if the value is not present, create a new value and store it in the map
+	// - remember to lock the value before updating it
+	// - if the value is deleted, try to obtain and update it again
 }
 
 func (as *avgAggrState) flushState(ctx *flushCtx, resetState bool) {
-	currentTimeMsec := int64(fasttime.UnixTimestamp()) * 1000
-	m := &as.m
-	m.Range(func(k, v interface{}) bool {
-		if resetState {
-			// Atomically delete the entry from the map, so new entry is created for the next flush.
-			m.Delete(k)
-		}
-
-		sv := v.(*avgStateValue)
-		sv.mu.Lock()
-		avg := sv.sum / float64(sv.count)
-		if resetState {
-			// Mark the entry as deleted, so it won't be updated anymore by concurrent pushSample() calls.
-			sv.deleted = true
-		}
-		sv.mu.Unlock()
-
-		key := k.(string)
-		ctx.appendSeries(key, "avg", currentTimeMsec, avg)
-		return true
-	})
+	// TODO: implement flushState
+	// hint:
+	// - if resetState is true, then delete the entry from the map
+	// - get the value as avgStateValue and calculate the avg
+	// - if resetState is true, update the deleted flag
+	// - remember to append the series to the ctx
 }
