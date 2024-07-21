c1d7ef3394cd707619aa9d25d3b8b075d137964e
leiwingqueen <leiwingqueen@gmail.com>
2024-07-21 19:08:15 +0800

    implement unique sample

--- lib/streamaggr/unique_samples.go
+++ lib/streamaggr/unique_samples.go
@@ -1,6 +1,7 @@
 package streamaggr
 
 import (
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/fasttime"
 	"sync"
 )
 
@@ -11,7 +12,8 @@ type uniqueSamplesAggrState struct {
 
 type uniqueSamplesStateValue struct {
 	mu sync.Mutex
-	// TODO: implement
+	// implement
+	mp      map[float64]struct{}
 	deleted bool
 }
 
@@ -20,9 +22,50 @@ func newUniqueSamplesAggrState() *uniqueSamplesAggrState {
 }
 
 func (as *uniqueSamplesAggrState) pushSamples(samples []pushSample) {
-	// TODO: implement unique_samples pushSamples
+	// implement unique_samples pushSamples
+	for _, sample := range samples {
+		for {
+			outputKey := getOutputKey(sample.key)
+			var value *uniqueSamplesStateValue
+			v, ok := as.m.Load(outputKey)
+			if !ok {
+				value = &uniqueSamplesStateValue{mp: make(map[float64]struct{}), deleted: false}
+				actual, loaded := as.m.LoadOrStore(outputKey, value)
+				if loaded {
+					value = actual.(*uniqueSamplesStateValue)
+				}
+			} else {
+				value = v.(*uniqueSamplesStateValue)
+			}
+			// update value
+			value.mu.Lock()
+			deleted := value.deleted
+			if !deleted {
+				value.mp[sample.value] = struct{}{}
+			}
+			value.mu.Unlock()
+			if !deleted {
+				break
+			}
+		}
+	}
 }
 
 func (as *uniqueSamplesAggrState) flushState(ctx *flushCtx, resetState bool) {
-	// TODO: implement unique_samples flushState
+	// implement unique_samples flushState
+	timestamp := int64(fasttime.UnixTimestamp()) * 1_000
+	as.m.Range(func(key, value any) bool {
+		k := key.(string)
+		v := value.(*uniqueSamplesStateValue)
+		if resetState {
+			as.m.Delete(key)
+		}
+		v.mu.Lock()
+		if resetState {
+			v.deleted = true
+		}
+		v.mu.Unlock()
+		ctx.appendSeries(k, "unique_samples", timestamp, float64(len(v.mp)))
+		return true
+	})
 }
