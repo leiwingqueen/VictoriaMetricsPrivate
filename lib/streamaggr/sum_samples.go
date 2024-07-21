5339200263da3e070917ff26a4a5fe4382f35569
leiwingqueen <leiwingqueen@gmail.com>
2024-07-21 19:28:40 +0800

    implement sum sample

--- lib/streamaggr/sum_samples.go
+++ lib/streamaggr/sum_samples.go
@@ -1,6 +1,7 @@
 package streamaggr
 
 import (
+	"github.com/VictoriaMetrics/VictoriaMetrics/lib/fasttime"
 	"sync"
 )
 
@@ -12,6 +13,7 @@ type sumSamplesAggrState struct {
 type sumSamplesStateValue struct {
 	mu sync.Mutex
 	// implement
+	sum     float64
 	deleted bool
 }
 
@@ -20,9 +22,50 @@ func newSumSamplesAggrState() *sumSamplesAggrState {
 }
 
 func (as *sumSamplesAggrState) pushSamples(samples []pushSample) {
-	// TODO: implement
+	// implement
+	for _, sample := range samples {
+		for {
+			outputKey := getOutputKey(sample.key)
+			var value *sumSamplesStateValue
+			v, ok := as.m.Load(outputKey)
+			if !ok {
+				value = &sumSamplesStateValue{sum: 0, deleted: false}
+				actual, loaded := as.m.LoadOrStore(outputKey, value)
+				if loaded {
+					value = actual.(*sumSamplesStateValue)
+				}
+			} else {
+				value = v.(*sumSamplesStateValue)
+			}
+			// update value
+			value.mu.Lock()
+			deleted := value.deleted
+			if !deleted {
+				value.sum += sample.value
+			}
+			value.mu.Unlock()
+			if !deleted {
+				break
+			}
+		}
+	}
 }
 
 func (as *sumSamplesAggrState) flushState(ctx *flushCtx, resetState bool) {
-	// TODO: implement
+	// implement
+	timestamp := int64(fasttime.UnixTimestamp()) * 1_000
+	as.m.Range(func(key, value any) bool {
+		k := key.(string)
+		v := value.(*sumSamplesStateValue)
+		if resetState {
+			as.m.Delete(key)
+		}
+		v.mu.Lock()
+		if resetState {
+			v.deleted = true
+		}
+		v.mu.Unlock()
+		ctx.appendSeries(k, "sum_samples", timestamp, v.sum)
+		return true
+	})
 }
