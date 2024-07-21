2df777d8681fb649fec1aec4012f46af72174eb8
leiwingqueen <leiwingqueen@gmail.com>
2024-07-21 18:59:21 +0800

    remove some code

--- lib/streamaggr/count_samples.go
+++ lib/streamaggr/count_samples.go
@@ -2,8 +2,6 @@ package streamaggr
 
 import (
 	"sync"
-
-	"github.com/VictoriaMetrics/VictoriaMetrics/lib/fasttime"
 )
 
 // countSamplesAggrState calculates output=count_samples, e.g. the count of input samples.
@@ -12,8 +10,8 @@ type countSamplesAggrState struct {
 }
 
 type countSamplesStateValue struct {
-	mu      sync.Mutex
-	n       uint64
+	mu sync.Mutex
+	// TODO: implement
 	deleted bool
 }
 
@@ -22,60 +20,9 @@ func newCountSamplesAggrState() *countSamplesAggrState {
 }
 
 func (as *countSamplesAggrState) pushSamples(samples []pushSample) {
-	for i := range samples {
-		s := &samples[i]
-		outputKey := getOutputKey(s.key)
-
-	again:
-		v, ok := as.m.Load(outputKey)
-		if !ok {
-			// The entry is missing in the map. Try creating it.
-			v = &countSamplesStateValue{
-				n: 1,
-			}
-			vNew, loaded := as.m.LoadOrStore(outputKey, v)
-			if !loaded {
-				// The new entry has been successfully created.
-				continue
-			}
-			// Use the entry created by a concurrent goroutine.
-			v = vNew
-		}
-		sv := v.(*countSamplesStateValue)
-		sv.mu.Lock()
-		deleted := sv.deleted
-		if !deleted {
-			sv.n++
-		}
-		sv.mu.Unlock()
-		if deleted {
-			// The entry has been deleted by the concurrent call to flushState
-			// Try obtaining and updating the entry again.
-			goto again
-		}
-	}
+	// TODO: implement
 }
 
 func (as *countSamplesAggrState) flushState(ctx *flushCtx, resetState bool) {
-	currentTimeMsec := int64(fasttime.UnixTimestamp()) * 1000
-	m := &as.m
-	m.Range(func(k, v interface{}) bool {
-		if resetState {
-			// Atomically delete the entry from the map, so new entry is created for the next flush.
-			m.Delete(k)
-		}
-
-		sv := v.(*countSamplesStateValue)
-		sv.mu.Lock()
-		n := sv.n
-		if resetState {
-			// Mark the entry as deleted, so it won't be updated anymore by concurrent pushSample() calls.
-			sv.deleted = true
-		}
-		sv.mu.Unlock()
-
-		key := k.(string)
-		ctx.appendSeries(key, "count_samples", currentTimeMsec, float64(n))
-		return true
-	})
+	// TODO: implement
 }
